4c90a7ab3b80b364d8186aed5454b7dba453aa56
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2023-03-29 11:34:05 +0530

    created a set of files

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <?php wp_head();?>
+</head>
+
+<body <?php body_class();?>>
+
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+
+  </a>
+</div>
+
